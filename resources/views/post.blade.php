fab206bc946ab25d4151e29d977c4a892f266757
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2021-09-13 21:43:13 +0200

    Section 3 episode 15

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,4 @@
-<!doctype html>
-
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
+<x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
         <div>
@@ -12,4 +7,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,4 @@
-<!doctype html>
-
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
+<x-layout>
     @foreach($posts as $post)
         <article class="{{ $loop->even ? 'mb-6' : '' }}">
             <h1>
@@ -12,4 +7,5 @@
             <div>{{ $post->excerpt }}</div>
         </article>
     @endforeach
-</body>
+</x-layout>
+
