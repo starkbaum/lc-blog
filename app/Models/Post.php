9fea05b879953c5184176ba0f582d52154b57499
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2021-09-13 21:02:52 +0200

    Section 2 epsode 12

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,26 +6,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path("/posts/"));
-
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+        return collect(File::files(resource_path("posts")))
+            ->map(function ($file) {
+                $document = YamlFrontMatter::parseFile($file);
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            });
     }
 
     public static function find($slug)
     {
-        if (! file_exists($path = resource_path("/posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, function () use ($path) {
-            return file_get_contents($path);
-        });
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('posts', [
         'posts' => Post::all(),
     ]);
