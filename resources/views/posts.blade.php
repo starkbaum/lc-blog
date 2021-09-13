553a8fed98b22b455ee394f5cbd4da5765046975
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2021-09-13 21:20:45 +0200

    Section 3 episode 14

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,12 +4,12 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-<?php foreach ($posts as $post) : ?>
-    <article>
-        <h1>
-            <a href="/posts/<?= $post->slug; ?>"><?= $post->title ?></a>
-        </h1>
-        <div><?= $post->excerpt ?></div>
-    </article>
-<?php endforeach; ?>
+    @foreach($posts as $post)
+        <article class="{{ $loop->even ? 'mb-6' : '' }}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            </h1>
+            <div>{{ $post->excerpt }}</div>
+        </article>
+    @endforeach
 </body>
