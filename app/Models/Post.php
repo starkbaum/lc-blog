565ef6509bdcbdac9323e9e66323afa9d2d8ebde
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2021-09-22 20:33:51 +0200

    Section 2 episode 24

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,17 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body'];
+    //protected $fillable = ['title', 'excerpt', 'body'];
 
-    //protected $guarded = ['id'];
+    protected $guarded = [];
 
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,6 +4,9 @@
             <h1>
                 <a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a>
             </h1>
+            <p>
+                <a href="#">{{ $post->category->name }}</a>
+            </p>
             <div>{{ $post->excerpt }}</div>
         </article>
     @endforeach
