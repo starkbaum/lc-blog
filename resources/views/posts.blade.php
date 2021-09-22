2af43f97af249c4f7e0358f19cc459e7acb02b8c
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2021-09-23 00:10:58 +0200

    Section 5 episode 31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,35 @@
+{{--<x-layout>--}}
+{{--    @foreach($posts as $post)--}}
+{{--        <article class="{{ $loop->even ? 'mb-6' : '' }}">--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a>--}}
+{{--            </h1>--}}
+{{--            <p>--}}
+{{--                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--            </p>--}}
+{{--            <div>{{ $post->excerpt }}</div>--}}
+{{--        </article>--}}
+{{--    @endforeach--}}
+{{--</x-layout>--}}
+
+
 <x-layout>
-    @foreach($posts as $post)
-        <article class="{{ $loop->even ? 'mb-6' : '' }}">
-            <h1>
-                <a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a>
-            </h1>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>{{ $post->excerpt }}</div>
-        </article>
-    @endforeach
+
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+    
 </x-layout>
 
