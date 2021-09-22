4f8e7f2387db9774c0e1d4dfa31cba6c431a2e38
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2021-09-22 20:43:40 +0200

    Section 4 episode 25

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
                 <a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a>
             </h1>
             <p>
-                <a href="#">{{ $post->category->name }}</a>
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
             <div>{{ $post->excerpt }}</div>
         </article>
